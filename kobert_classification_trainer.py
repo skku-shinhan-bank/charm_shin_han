d03f424389129600728cc0a221476b614221a14a
hanjungwoo <ajtwlswjddnv1102@gmail.com>
2021-06-24 17:27:39 +0900

    feat:분리

--- kobert_classification_trainer.py
+++ kobert_classification_trainer.py
@@ -3,56 +3,12 @@ from kobert.pytorch_kobert import get_pytorch_kobert_model
 import random
 import torch
 import gluonnlp as nlp
-# from kobert_classifier import KoBERTClassifier
+from kobert_classifier import KoBERTClassifier
+from kobert_dataset import KoBERTDataset
 from transformers import AdamW
 from transformers.optimization import get_cosine_schedule_with_warmup
 from torch import nn
 from tqdm import tqdm, tqdm_notebook
-from torch.utils.data import Dataset
-import numpy as np
-
-class KoBERTDataset(Dataset):
-  def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len,
-              pad, pair):
-    transform = nlp.data.BERTSentenceTransform(
-        bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
-
-    self.sentences = [transform([i[sent_idx]]) for i in dataset]
-    self.labels = [np.int32(i[label_idx]) for i in dataset]
-
-  def __getitem__(self, i):
-    return (self.sentences[i] + (self.labels[i], ))
-
-  def __len__(self):
-    return (len(self.labels))
-
-class KoBERTClassifier(nn.Module):
-    def __init__(self,
-                 bert,
-                 hidden_size = 768,
-                 num_classes=None,
-                 dr_rate=None,
-                 params=None):
-        super(KoBERTClassifier, self).__init__()
-        self.bert = bert
-        self.dr_rate = dr_rate
-                 
-        self.classifier = nn.Linear(hidden_size , num_classes)
-        if dr_rate:
-            self.dropout = nn.Dropout(p=dr_rate)
-    
-    def gen_attention_mask(self, token_ids, valid_length):
-        attention_mask = torch.zeros_like(token_ids)
-        for i, v in enumerate(valid_length):
-            attention_mask[i][:v] = 1
-        return attention_mask.float()
-
-    def forward(self, token_ids, valid_length, segment_ids):
-        attention_mask = self.gen_attention_mask(token_ids, valid_length)
-        _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device), return_dict=False)
-        if self.dr_rate:
-            out = self.dropout(pooler)
-        return self.classifier(out)
 
 class KobertClassficationTrainer:
   def __init__(self):
@@ -144,4 +100,4 @@ class KobertClassficationTrainer:
             test_acc += self.calc_accuracy(out, label)
         print("epoch {} test acc {}".format(e+1, test_acc / (batch_id+1)))
 
-    return model
+    torch.save(model.state_dict(), model_output_path)
