45e0cc752c81ad4aee5ee344f9d8c6f03fda82e4
leeyejin1231 <leeyejin1231>
2021-07-12 15:12:30 +0900

    feat:max_seq_len 변경

--- model/koelectra_classifier.py
+++ model/koelectra_classifier.py
@@ -57,26 +57,6 @@ class KoElectraClassifier(ElectraPreTrainedModel):
 
       return outputs  # (loss), (logits), (hidden_states), (attentions)
 
-    def koelectra_input(tokenizer, str, device = None, max_seq_len = 512):
-      index_of_words = tokenizer.encode(str)
-      # token_type_ids = [0] * len(index_of_words)
-      attention_mask = [1] * len(index_of_words)
-
-      # Padding Length
-      padding_length = max_seq_len - len(index_of_words)
-
-      # Zero Padding
-      index_of_words += [0] * padding_length
-      # token_type_ids += [0] * padding_length
-      attention_mask += [0] * padding_length
-
-      data = {
-        'input_ids': torch.tensor([                    
-        ]).to(device),
-        'attention_mask': torch.tensor([attention_mask]).to(device),
-      }
-      return data
-
 class KoElectraClassificationHead(nn.Module):
 
   def __init__(self, config, num_labels):
