c272911036a904076763d4623bdb564de45b71a1
leeyejin1231 <leeyejin1231>
2021-07-22 19:58:05 +0900

    feat: 어렵다 증말..ㅠㅠ

--- koelectra_vocab.py
+++ koelectra_vocab.py
@@ -6,13 +6,13 @@ class KoElectra_vocab:
         pass
 
     def Wordpeiece(corpus_file, limit_alphabet, vocab_size):
-        parser = argparse.ArgumentParser()
+        # parser = argparse.ArgumentParser()
 
         # parser.add_argument(corpus_file)
         # parser.add_argument("--vocab_size", type=int, default=22000) # 만들 Vocab의 숫자 
         # parser.add_argument("--limit_alphabet", type=int, default=6000)
 
-        args = parser.parse_args()
+        # args = parser.parse_args()
 
         tokenizer = BertWordPieceTokenizer(
             vocab_file=None,
@@ -24,9 +24,9 @@ class KoElectra_vocab:
         )
 
         tokenizer.train(
-            files=[corpus_file],
+            files=corpus_file,
             limit_alphabet=limit_alphabet,
             vocab_size=vocab_size
         )
 
-        tokenizer.save("./ch-{}-wpm-{}-pretty".format(args.limit_alphabet, args.vocab_size),True)
+        tokenizer.save("./ch-{}-wpm-{}-pretty".format(limit_alphabet, vocab_size),True)
