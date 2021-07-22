49d123b42bf0dfccfcf19fc222fe654d5d7b13d6
Hans-0101 <hyhsumin@naver.com>
2021-07-22 20:18:36 +0900

    feat:vocab 수정

--- wordpiece_vocab.py
+++ wordpiece_vocab.py
@@ -18,7 +18,7 @@ class WordpieceVocab :
             lowercase=False,
             wordpieces_prefix="##"
         )
-        tokenizer = BertWordPieceTokenizer.from_pretrained("monologg/koelectra-base-v3-discriminator")
+        # tokenizer = BertWordPieceTokenizer.from_pretrained("monologg/koelectra-base-v3-discriminator")
 
         tokenizer.train(
             files=[self.corpus_file],
