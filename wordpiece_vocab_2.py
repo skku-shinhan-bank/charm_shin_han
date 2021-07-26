cdfa6ef4ce79f3ebed40e7a42b5af0fb5a65e761
Hans-0101 <hyhsumin@naver.com>
2021-07-26 15:30:02 +0900

    feat:vocab test

--- wordpiece_vocab_2.py
+++ wordpiece_vocab_2.py
@@ -33,19 +33,36 @@ class WordpieceVocab :
             os.mkdir(checkpoint_path)
         tokenizer.save_model("./checkpoint")
 
-    def retrain_tokenizer(self, config):
+		# electra_config = ElectraConfig.from_pretrained("monologg/koelectra-base-v3-discriminator")
+		# classification_model = KoElectraClassifier.from_pretrained(pretrained_model_name_or_path = "monologg/koelectra-base-v3-discriminator", config = electra_config, num_labels = config.num_label)
+		# new_tokenizer = AutoTokenizer.from_pretrained("monologg/koelectra-base-v3-discriminator")
 
-		electra_config = ElectraConfig.from_pretrained("monologg/koelectra-base-v3-discriminator")
-		classification_model = KoElectraClassifier.from_pretrained(pretrained_model_name_or_path = "monologg/koelectra-base-v3-discriminator", config = electra_config, num_labels = config.num_label)
-        new_tokenizer = AutoTokenizer.from_pretrained("monologg/koelectra-base-v3-discriminator")
-        origin_tokens = []
-        f = open("/checkpoint/vocab.txt", 'r')
-        while True :
-            line = f.readline()
-            if not line :
-                break
-            origin_tokens.append(line)
-        f.close()
-        new_tokens=origin_tokens[5:]
-        new_tokenizer.add_tokens(new_tokens)
+		origin_tokens = []
+		f = open("/content/checkpoint/vocab.txt", 'r')
+		while True :
+			line = f.readline()
+			if not line :
+				break
+			origin_tokens.append(line)
+		f.close()
+		new_tokens=origin_tokens[5:]
+        print(new_tokens)
+		# new_tokenizer.add_tokens(new_tokens)
+		# classification_model.resize_token_embeddings(len(tokenizer))
+
+    # def retrain_tokenizer(self, config):
+
+	# 	electra_config = ElectraConfig.from_pretrained("monologg/koelectra-base-v3-discriminator")
+	# 	classification_model = KoElectraClassifier.from_pretrained(pretrained_model_name_or_path = "monologg/koelectra-base-v3-discriminator", config = electra_config, num_labels = config.num_label)
+    #     new_tokenizer = AutoTokenizer.from_pretrained("monologg/koelectra-base-v3-discriminator")
+    #     origin_tokens = []
+    #     f = open("/checkpoint/vocab.txt", 'r')
+    #     while True :
+    #         line = f.readline()
+    #         if not line :
+    #             break
+    #         origin_tokens.append(line)
+    #     f.close()
+    #     new_tokens=origin_tokens[5:]
+    #     new_tokenizer.add_tokens(new_tokens)
 
