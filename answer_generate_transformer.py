f0bbab5f416a23417f0d622fb86aa165fd4c28d7
Park Jubong <jubong@bagjubong-ui-MacBookPro.local>
2021-08-10 02:11:53 +0900

    feat: ver2

--- answer_generate_transformer.py
+++ answer_generate_transformer.py
@@ -194,7 +194,7 @@ class MultiHeadAttention(tf.keras.layers.Layer):
   def create_look_ahead_mask(x):
     seq_len = tf.shape(x)[1]
     look_ahead_mask = 1 - tf.linalg.band_part(tf.ones((seq_len, seq_len)), -1, 0)
-    padding_mask = create_padding_mask(x) # 패딩 마스크도 포함
+    padding_mask = MultiHeadAttention.create_padding_mask(x) # 패딩 마스크도 포함
     return tf.maximum(look_ahead_mask, padding_mask)
 
   def decoder_layer(dff, d_model, num_heads, dropout, name="decoder_layer"):
