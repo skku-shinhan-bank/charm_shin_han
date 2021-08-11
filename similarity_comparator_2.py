f56a6fe6f9a79267131d38a80bf0900ef4d43840
Hans-0101 <hyhsumin@naver.com>
2021-08-11 18:01:38 +0900

    feat:오류수정

--- similarity_comparator_2.py
+++ similarity_comparator_2.py
@@ -43,7 +43,7 @@ class SimilarityComparator:
   def mean_pooling(self, model_output, attention_mask):
     #Mean Pooling - Take attention mask into account for correct averaging
     token_embeddings = model_output[0] #First element of model_output contains all token embeddings
-    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    input_mask_expanded = attention_mask.squeeze(-1).expand(token_embeddings.size()).float()
     sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
     sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
     return sum_embeddings / sum_mask
