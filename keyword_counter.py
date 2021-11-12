87385afb2d6f035b939677832697894578b51501
leeyejin1231 <leeyejin1231>
2021-11-12 11:08:16 +0900

    feat:change label name

---
+++ keyword_counter.py
@@ -0,0 +1,14 @@
+import collections
+import numpy as np
+import pandas as pd
+from operator import itemgetter
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+class KeywordCouter:
+    def __init__(self):
+        return
+    
+    def makecount(self, issue0, issue1, issue2, issue3, issue4, issue5):
+        
+        return
