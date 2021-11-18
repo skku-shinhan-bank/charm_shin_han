67cf375dff14daf0a53d396241c746fbfcc13584
leeyejin1231 <leeyejin1231>
2021-11-19 04:29:23 +0900

    feat:월별급상승

--- sudden_keyword.py
+++ sudden_keyword.py
@@ -1,44 +1,47 @@
 import pandas as pd
 import numpy as np
-from .keyword_extracter import KeywordExtracter
+# from .keyword_extracter import KeywordExtracter
 
 class SuddenKeyword:
     def __init__(self):
-        self.ke = KeywordExtracter()
+        # self.ke = KeywordExtracter()
         return
 
     def make_top10(self, data_path):
         top10 = []
 
-        data = pd.read_excel(data_path)
-        month, month_index = self.month_classifier(data)
+        # data = pd.read_excel(data_path)
+
+        # month, month_index = self.month_classifier(data)
         
-        for i in month_index:
-            top10.append(self.ke.analyze(list(month[i])))
+        # for i in month_index:
+        #     top10.append(self.ke.analyze(list(month[i])))
 
 
         return top10
 
-    def month_classifier(self, data):
-            month_index = []
-            month_data = []
+    def month_classifier(self, data_path):
+        data = pd.read_excel(data_path)
 
+        month_index = []
+        month_data = []
+
+        for i in range(len(data)):
+            month_index.append(str(data['일자'][i])[:7])
+
+        month_index = set(month_index)
+
+        for m in month_index:
+            temp = []
             for i in range(len(data)):
-                month_index.append(str(data['일자'][i])[:7])
+                if str(data['일자'][i])[:7] == m:
+                    temp.append(data['review'][i])
+            month_data.append(temp)
 
-            month_index = set(month_index)
+        month = pd.DataFrame(month_data)
+        n = 0
+        for i in month_index:
+            month = month.rename(columns={n:i})
+            n += 1
 
-            for m in month_index:
-                temp = []
-                for i in range(len(data)):
-                    if str(data['일자'][i])[:7] == m:
-                        temp.append(data['review'][i])
-                month_data.append(temp)
-
-            month = pd.DataFrame(month_data)
-            n = 0
-            for i in month_index:
-                month = month.rename(columns={n:i})
-                n += 1
-
-            return month, month_index
+        return month, month_index
